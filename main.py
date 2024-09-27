5429db32feeb63743be5bc222bc22e7a98dbd76a
ivangb6 <ivangb6@gmail.com>
2024-09-27 04:57:12 +0000

    Clean up

--- main.py
+++ main.py
@@ -44,40 +44,13 @@ async def on_message(message):
       messageToSend = jeremyShouts[random.randInt(0, 1)]
 
     if 'together' in message.content:
-      messageToSend = f'Philosophy is done best in community.\n\t\t- Jeremy Reid'
+      messageToSend = f'Philosophy is done best in community.\n\t\t-Jeremy Reid'
     if 'someone wants' in msgFormat:
-      messageToSend = f'Philosophy is done best in community.\n\t\t- Jeremy Reid'
+      messageToSend = f'Philosophy is done best in community.\n\t\t-Jeremy Reid'
     if 'who wants' in msgFormat:
-      messageToSend = f'Philosophy is done best in community.\n\t\t- Jeremy Reid'
-    
-    ########--------- REFERENCES ---------########
-    # if '<' == message.content:
-    #   await message.channel.send(shouts[random.randint(0, 2)])
-    # if '<<' == message.content:
-    #   await message.channel.send(shouts[random.randint(0, 2)])
-    # if '<--' == message.content:
-    #   await message.channel.send(shouts[random.randint(0, 2)])
-    # if '<---' == message.content:
-    #   await message.channel.send(shouts[random.randint(0, 2)])
-    # if '...' == message.content:
-    #     await message.channel.send(f"te falta algo {author}?")
-    
-    # if 'blingers' in msgFormat:
-    #   await message.channel.send('BLINGERS PILINGERS ASSEMBLE:bangbang:')
-    # if 'lil blinger' in msgFormat:
-    #     await message.channel.send(greetings[random.randint(0,2)])
-    # if 'hola' in msgFormat:
-    #   await message.channel.send(greetings[random.randint(0,2)])
-    # if 'ora' in msgFormat:
-    #   currentTime = time.strftime("%H:%M:%S", time.localtime())
-    #   await message.channel.send(f"son las {currentTime}")
-    # if 'puto' in msgFormat:
-    #   await message.channel.send(f"puto tu {author}")
-    # if 'madre' in msgFormat:
-    #   await message.channel.send(f"la tuya {author}")
-    # if 'shit' in msgFormat:
-    #   await message.channel.send(f"A que hora pasas por el pan {author}")
+      messageToSend = f'Philosophy is done best in community.\n\t\t-Jeremy Reid'
 
+    # Only send messageToSend if the string is not empty
     if messageToSend:
       await message.channel.send(messageToSend)
 
